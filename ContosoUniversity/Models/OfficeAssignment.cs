41eb8687b4603806d552e85fcfdf1df4d29e4035
ST-PC\ST <vanhongit@gmail.com>
2019-04-02 15:55:23 +0700

    Create a complex data model

---
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class OfficeAssignment
+    {
+        [Key] // There's a one-to-zero-or-one relationship between the Instructor and the OfficeAssignment entities
+        public int InstructorID { get; set; } // name doesn't follow the ID or classnameID & used to identify it as the key
+
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public Instructor Instructor { get; set; }
+    }
+}
